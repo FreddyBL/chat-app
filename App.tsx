36a9ab6922a19c944f7d380034802adfeb3c330b
FreddyBL <84300272+FreddyBL@users.noreply.github.com>
2021-09-22 05:48:04 -0600

    Implemented user registration, added user GraphQL schema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -9,12 +9,53 @@ import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
 import { withAuthenticator } from 'aws-amplify-react-native'
 
+import { Auth, API } from 'aws-amplify';
+import { graphqlOperation } from 'aws-amplify';
+import { getUser } from './src/graphql/queries';
+
+import { createUser } from './src/graphql/mutations';
 Amplify.configure(config)
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser( {bypassCache: true});
+      if(userInfo != null){
+        
+        const id = userInfo.attributes.sub;
+
+        const userData = await API.graphql(graphqlOperation(
+          getUser, {
+            id,
+          }));
+
+          if(userData.data.getUser){
+            console.log("User is already registered")
+          }
+          else{
+            // Register user
+            console.log("Trying to register: ...");
+            const newUser = {
+              id,
+              name: userInfo.username,
+              email: userInfo.attributes.email
+            }
+            console.log(newUser);
+            await API.graphql(graphqlOperation(createUser, 
+            {input: newUser}
+            ));
+            console.log("User has been registered!");
+
+          }
+      }
+    }
+
+    fetchUser();
+  }, []);
   if (!isLoadingComplete) {
     return null;
   } else {
